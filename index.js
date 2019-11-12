13c3e106f711531d7592a4501517e5051c9b2931
Lavinia <laviniatulbure11@gmail.com>
2019-11-12 23:11:43 +0100

    Created components directory with HTMLs

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+let mainPageName = 'mainPage';
 
-let loadPartial = (partialName) => {
-  fetch(partialName + ".html").then(result => result.text()).then((textResult) => {
-    document.write(textResult);
-    // get partial div
-    // thatDiv.innerHTML = textResult
+let loadPartial = (componentName) => {
+  fetch('components/' + componentName + '.html').then(result => result.text()).then((textResult) => {
+    // Remove selected class from all the top buttons
+    document.querySelectorAll('#top-buttons > li').forEach((listItem) => {
+      listItem.classList.remove('selected');
+    });
+  
+    // Add selected class to the clicked button, if it's not the main page
+    if (componentName !== mainPageName) {
+      document.getElementById(componentName).classList.add('selected');
+    }
+    
+    // Load component data
+    // document.getElementById("componentMount").innerHTML = textResult;
+    document.querySelector('#componentMount').innerHTML = textResult;
   });
 };
+
+window.onload = () => {
+  // Initialize with the main page
+  loadPartial(mainPageName);
+}
